5759ce933763841067dc1af31ee897a94933775b
BillyCarbajal <116075944+BillyCarbajal@users.noreply.github.com>
2022-12-12 10:51:19 +0100

    Update script-nfs.sh

--- script-nfs.sh
+++ script-nfs.sh
@@ -5,7 +5,7 @@ echo "/var/www       192.168.100.3(rw,sync,no_root_squash,no_subtree_check) 192.
 sed -i 's/^listen .*$/listen = 192.168.200.5:9000/' /etc/php/7.4/fpm/pool.d/www.conf
 service nfs-kernel-server restart
 cd /var/www/html
-wget wget https://downloads.joomla.org/es/cms/joomla4/4-2-5/Joomla_4-2-5-Stable-Full_Package.zip
+wget https://downloads.joomla.org/es/cms/joomla4/4-2-5/Joomla_4-2-5-Stable-Full_Package.zip
 unzip Joomla_4-2-5-Stable-Full_Package.zip
 rm Joomla_4-2-5-Stable-Full_Package.zip
 chown -R www-data:www-data ../html
